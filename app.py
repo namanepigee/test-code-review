13fb3b743c1f0ee891a14fa035d075fb44812b48
namanepigee <naman.gupta@epigeeus.com>
2025-08-11 16:27:00 +0530

    addition function added

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ print('helooo')
 print('Hello from feature branch')
 
 print('patch 2')
+
+print('patch 3') 
+
+def add_numbers(a, b):
+    return a + b
+
+# Example usage
+result = add_numbers(5, 3)
+print(f"The sum of 5 and 3 is: {result}")
